b211df55b72b25f9d6192e7b6696dc3231cfda6a
aiautomation111 <aiautomation111.3@gmail.com>
2025-08-25 23:42:08 +0300

    Update HeroSection.tsx

--- components/web/pages/HeroSection.tsx
+++ components/web/pages/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         {/* القسم الأيسر: المحتوى النصي */}
         <div className="flex flex-col gap-8 pr-0 md:pr-10">
           <h1 className="text-5xl md:text-6xl font-bold leading-tight">
-  Not Just Mobile Experts.Its An <span className="text-red-500">Experience!</span>
+  Not Just Mobile Experts.Its An <span className="text-red-500 ">Experience!</span>
 </h1>
           <p className="text-gray-400 text-lg">
 Our Digital Solution Are The Defference Between You Owning a Job And Owning Your Time. 
